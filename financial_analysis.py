a500c2739bc4a1894bc9462ed80a6759753b8dd1
Atharva30077 <atharva303030@gmail.com>
2026-03-10 22:13:21 +0530

    financial_analysis.py

--- financial_analysis.py
+++ financial_analysis.py
@@ -1,1 +1,13 @@
+def analyze_finances(income, expenses, savings, debt):
 
+    savings_ratio = (income - expenses) / income if income else 0
+
+    result = {
+        "income": income,
+        "expenses": expenses,
+        "savings": savings,
+        "debt": debt,
+        "savings_ratio": savings_ratio
+    }
+
+    return result
