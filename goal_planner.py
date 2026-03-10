48a1b7ead5fa8cc5d70508939c91638c71f37ba4
Atharva30077 <atharva303030@gmail.com>
2026-03-10 22:13:52 +0530

    goal_planner.py

--- goal_planner.py
+++ goal_planner.py
@@ -1,1 +1,10 @@
+def goal_planner(goal, amount, years):
 
+    monthly_savings = amount / (years * 12)
+
+    return {
+        "goal": goal,
+        "target_amount": amount,
+        "years": years,
+        "monthly_savings_required": monthly_savings
+    }
